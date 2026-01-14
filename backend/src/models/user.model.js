2e22bea733c8a4bc327d02e28edb16252f996919
Abhijeet Swami <abhijeets.swami@gmail.com>
2026-01-14 18:14:13 +0530

    feat(user): add User model

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+import argon2 from "argon2";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+      lowercase: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+  },
+  { timestamps: true },
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await argon2.hash(this.password);
+  next();
+});
+
+userSchema.methods.verifyPassword = async function (password) {
+  return await argon2.verify(this.password, password);
+};
+
+const User = mongoose.model("User", userSchema);
+export default User;
