4e8f0320169f09d04c71a8226c40e9c3e614a705
Akshit Ahuja <akshitahuja1322@gmail.com>
2026-02-03 15:02:27 +0530

    refactor: remove socket connection from CampFeed component

--- frontend/src/pages/CampFeed.jsx
+++ frontend/src/pages/CampFeed.jsx
@@ -1,76 +1,33 @@
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import FeedHeader from "../components/FeedHeader";
 import FloatingCreateButton from "../components/FloatingCreateButton";
 import CreatePostModal from "../components/CreatePostModal";
 import { useParams } from "react-router-dom";
 import PostCard from "../components/PostCard";
 import { CampContext } from "../context/authContext";
-import { socket } from "../utils/socket";
 
 const CampFeed = () => {
   const { id } = useParams();
 
-  const { posts, messagesByPost, setMessagesByPost } = useContext(CampContext);
-
-  useEffect(() => {
-    if (!posts.length) return;
-    socket.connect();
-
-    posts.forEach((post) => {
-      socket.emit("joinPost", post._id);
-    });
-
-    socket.on("message", ({ message }) => {
-      setMessagesByPost((prev) => ({
-        ...prev,
-        [message.postId]: [...(prev[message.postId] || []), message],
-      }));
-    });
-
-    socket.on("edit-message", ({ update }) => {
-      setMessagesByPost((prev) => ({
-        ...prev,
-        [update.postId]: prev[update.postId]?.map((m) =>
-          m._id === update._id ? update : m,
-        ),
-      }));
-    });
-
-    socket.on("delete-message", ({ messageId, postId }) => {
-      setMessagesByPost((prev) => ({
-        ...prev,
-        [postId]: prev[postId]?.filter((m) => m._id !== messageId),
-      }));
-    });
-
-    return () => {
-      socket.off("message");
-      socket.off("edit-message");
-      socket.off("delete-message");
-      socket.disconnect();
-    };
-  }, [posts, setMessagesByPost]);
+  const { posts } = useContext(CampContext);
 
   return (
     <main className="min-h-screen w-full flex flex-col items-center pb-32">
-      <div className="w-full max-w-3xl">
+      <div className="w-full max-w-full sm:max-w-xl md:max-w-2xl xl:max-w-3xl">
         <FeedHeader />
       </div>
 
-      {posts.length === 0 ? (
-        <p className="text-center text-[#a3a3a3] mt-10">
-          No posts yet. Be the first one 👀
-        </p>
-      ) : (
-        posts.map((post) => (
-          <PostCard
-            campId={id}
-            key={post._id}
-            post={post}
-            messagesByPost={messagesByPost[post._id] || []}
-          />
-        ))
-      )}
+      <div className="flex flex-col gap-2 w-full max-w-full sm:max-w-xl md:max-w-2xl xl:max-w-3xl">
+        {posts.length === 0 ? (
+          <p className="text-center text-[#a3a3a3] mt-10">
+            No posts yet. Be the first one 👀
+          </p>
+        ) : (
+          posts.map((post) => (
+            <PostCard campId={id} key={post._id} post={post} />
+          ))
+        )}
+      </div>
 
       <FloatingCreateButton />
 
